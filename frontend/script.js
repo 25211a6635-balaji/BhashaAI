d7f522380d96acbcd54e0612797211828bd6639d
Balaji Chole <25211a6635@bvrit.ac.in>
2026-09-14 23:38:41 +0530

    Prepare frontend for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -374,7 +374,7 @@ async function detectDialect() {
 
         const response =
             await fetch(
-                "http://127.0.0.1:8000/predict",
+                "/predict", ...)
                 {
                     method: "POST",
 
